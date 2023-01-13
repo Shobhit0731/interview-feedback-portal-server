83bece5d46d2c1470c45e7279e31e1c0afcd0ce0
vibhorden <Vibhor.garg@dentsu.com>
2023-01-13 22:10:54 +0530

    svr boilerplate

---
+++ FakeData.js
@@ -0,0 +1,14 @@
+let users = [
+    {
+        name: 'Rohan',
+        age:  20,
+        married: false
+    },
+    {
+        name: 'Sohan',
+        age:  30,
+        married: true
+    }
+];
+
+module.exports = { users };
